d9064feaacc89cee19d208a1efc33df240cf8282
ArpitaDeb <arpitadeb1990@gmail.com>
2021-02-20 11:46:05 -0500

    desktop app image perfect

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import ReactImageFallback from "react-image-fallback";
 import { Divider, Image, Card } from 'semantic-ui-react';
 
 class PostList extends React.Component {
-
+  state = {
+    src: '',
+    fallbackSrc : `https://assets.your.md/conditions/lymphoedema/card/lymphoedema-female-card.jpg`,
+    loaded : false,
+    error: false
+  }
   componentDidMount() {
     this.props.fetchPosts();
   }
+  onImageLoaded = () => {
+    this.setState({ loaded : true });
+  }
+  onImageError = () => {
+    this.setState({ error : true });
+  }
+  addDefaultSrc(ev){
+    ev.target.src = 'https://assets.your.md/conditions/lymphoedema/card/lymphoedema-female-card.jpg'
+  }
   renderList() {
+    const { src, loaded, fallbackSrc, error} = this.state;
     console.log(this.props.posts);
     return this.props.posts.map(post => {
+      let src = post.image;
+      //let imgSrc = (!error) ? src : fallbackSrc;
       return (
           <Card>
           <div className="item">
-            <Image className="middle aligned" size='small' src={post.image} />
+            {src ? <Image className="middle aligned" size='small' src={post.image}  onError= {this.addDefaultSrc} /> : <Image className="middle aligned" size='small' src={fallbackSrc}  onError= {this.addDefaultSrc} />}
             <Divider hidden />
             <div className="content">
               <div className="description">
